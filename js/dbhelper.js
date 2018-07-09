9391fe19358b2b8a51717594f322bbce156a2763
Alice <32536678+alicedxb@users.noreply.github.com>
2018-07-09 14:19:11 +0400

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,9 +7,11 @@ class DBHelper {
    * Database URL.
    * Change this to restaurants.json file location on your server.
    */
-  static get DATABASE_URL() {
+    static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    // return `http://localhost:${port}/data/restaurants.json`;
+    // Uncomment the previous two lines to make the project work on a local server
+    return `https://alicedxb.github.io/restaurant-app/data/restaurants.json`;
   }
 
   /**
