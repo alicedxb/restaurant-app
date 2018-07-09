02b9d8a4355832883157d001e86bb17db1253797
Alice <32536678+alicedxb@users.noreply.github.com>
2018-07-09 14:56:05 +0400

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -152,7 +152,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./restaurant-app/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
   
   /**
